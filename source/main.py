747fb872bfbd3494ff9ae5746749fc9dd102279f
taylor-w <taylorwood39@gmail.com>
2020-10-11 14:27:12 -0500

    Add 'dis' function

--- source/main.py
+++ source/main.py
@@ -6,11 +6,30 @@ import discord
 #Define main fxn
 def main():
     rng()
+    dis()
 
 #Define rng fxn
 def rng():
     task = str(input('What is the task at hand? '))
     prob = (random.random() * 100)
     print('The likelihood of ' + task + ' is ' + format(prob, '.2f') + '%.')
+
+#Define dis fxn
+def dis():
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    @client.event
+    async def on_msg(msg):
+        if msg.author == client.user:
+            return
+        
+        if msg.content.startswith('$hello'):
+            await message.channel.send('Howdy!')
+
+    client.run('PIZZA')
 
 main()
