c4b5d61811e69c14e32a1f46801bf500a5dd36e2
taylor-w <taylorwood39@gmail.com>
2020-10-11 13:15:48 -0500

    Add comments to 'main.py'

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#Main
+#Import random library
 import random
 
+#Define main fxn
 def main():
     rng()
 
+#Define rng fxn
 def rng():
     task = str(input('What is the task at hand? '))
     prob = (random.random() * 100)
