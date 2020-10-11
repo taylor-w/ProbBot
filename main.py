b8ed498b602a31488a32fcffbf69225d13089b6a
taylor-w <taylorwood39@gmail.com>
2020-10-11 13:14:09 -0500

    Add main file functionality

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Main
+import random
+
+def main():
+    rng()
+
+def rng():
+    task = str(input('What is the task at hand? '))
+    prob = (random.random() * 100)
+    print('The likelihood of ' + task + ' is ' + format(prob, '.2f') + '%.')
+
+main()
